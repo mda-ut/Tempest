394e9aa85d7b2c8f63ce9e9b7338bc86462fbffa
daylightSequence <michael.gu@mail.utoronto.ca>
2016-02-29 19:13:17 -0500

    Update pid.c
    Adjusted function to fit delta_time term. This delta time term is based on TIMER_RATE_IN_HZ from settings.h

--- fpga/fpga_sw/src/pid.c
+++ fpga/fpga_sw/src/pid.c
@@ -20,7 +20,7 @@ void PID_Reset (Controller_PID* PID) {
 void PID_Update (Controller_PID* PID, double value) {
 // updates the controller with a single reading
     PID->P = value;
-    PID->I = (1-PID->Alpha)*PID->I + value; 
+    PID->I = (1-PID->Alpha)*PID->I * (1/TIMER_RATE_IN_HZ) + value; 
     //(1-PID->Alpha)*PID->I should be multiplied by dt, time difference between 2 calls of PID_Update
     
     int i; 
@@ -32,9 +32,11 @@ void PID_Update (Controller_PID* PID, double value) {
     double temp = 0;
     if (PID->num_values >= PID_NUM_OLD_VALUES-1) { // if enough values accumulated
         // take the average difference between old valuse 
-        //for (i = 1; i < PID_NUM_OLD_VALUES; i++) temp += PID->old_values[i-1] - PID->old_values[i];
+        // for (i = 1; i < PID_NUM_OLD_VALUES; i++) temp += PID->old_values[i-1] - PID->old_values[i];
         temp = PID -> old_values[0] - PID-> old_values[PID_NUM_OLD_VALUES-1];
-        PID->D = (float)temp / PID_NUM_OLD_VALUES;
+        // change from Victor version:
+        // made it average rise / dt, where dt is 1/TIMER_RATE_HZ
+        PID->D = (float)temp / PID_NUM_OLD_VALUES * TIMER_RATE_IN_HZ;
     } else {
         PID->D = 0;
         PID->num_values++; // only increment here to avoid overflow
